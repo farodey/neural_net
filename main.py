b222d8bedb56b9f1ec3387f60427d95233a0b741
farodey <farodey@yandex.ru>
2019-11-05 15:20:05 +0300

    naive_matrix_vector_dot

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ def naive_add_matrix_and_vector(x, y):
     return x
 
 
+# Скалярное произведение двух векторов
 def naive_vector_dot(x, y):
     assert len(x.shape) == 1
     assert len(y.shape) == 1    # Убедиться, что x и y — векторы Numpy
@@ -31,6 +32,18 @@ def naive_vector_dot(x, y):
     z = 0.
     for i in range(x.shape[0]):
         z += x[i] * y[i]
+    return z
+
+
+def naive_matrix_vector_dot(x, y):
+    assert len(x.shape) == 2            # Убедиться, что x — матрица Numpy
+    assert len(y.shape) == 1            # Убедиться, что y — вектор Numpy
+    assert x.shape[1] == y.shape[0]     # Первое измерение x должно совпадать с нулевым измерением y!
+
+    z = np.zeros(x.shape[0])            # Эта операция вернет вектор с нулевыми элементами, имеющий ту же форму, что и y
+    for i in range(x.shape[0]):
+        for j in range(x.shape[1]):
+            z[i] += x[i, j] * y[j]
     return z
 
 
